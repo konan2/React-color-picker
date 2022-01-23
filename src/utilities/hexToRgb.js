4e46bf1a44defcb6414ed4f1cadad9ee56892089
konan2 <boxsizing@gmail.com>
2022-01-23 17:39:32 +0200

    Updates

--- src/utilities/hexToRgb.js
+++
@@ -1,8 +0,0 @@
-
-export const hexToRgb = (hex) => {
-    let rgb = hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
-    ,(m, r, g, b) => '#' + r + r + g + g + b + b)
-    .substring(1).match(/.{2}/g)
-    .map(x => parseInt(x, 16))
-    return rgb
-}
